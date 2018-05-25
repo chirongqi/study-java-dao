0779bb1bb37dff1c072f2f85f01748bcb7a794de
rongqi.chi <rongqi.chi@weimob.com>
2018-05-25 15:43:27 +0800

    update

---
+++ study-java-dao-singleMybatis/src/main/java/com/chirq/study/jdbc/App.java
@@ -0,0 +1,12 @@
+package com.chirq.study.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
